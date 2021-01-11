937075935eb6369f91f8196780aab5bcf2788eca
sondossamii <sondos.sami.abuzeid@gmail.com>
2021-01-11 21:42:36 +0200

    Add <Banner/> and <Artists/> to 'Home' Component

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import Artists from "./artists";
+import Banner from "./banner";
+
+const Home = ()=> {
+    return(
+        <div>
+            <Banner/>
+            <Artists/>
+        </div>
+    )
+}
+
+export default Home
