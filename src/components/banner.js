26df5568755782650e1c16f88b041ea9a87597a4
sondossamii <sondos.sami.abuzeid@gmail.com>
2021-01-13 20:34:46 +0200

    Add my name to banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -9,8 +9,15 @@ const Banner = ()=> {
     return (
         <div style={{width: '100%', height: '100vh'}}>
             <div style={bgStyle}>
-                <div className="row justify-content-center align-items-center w-100 h-100">
+                <div className="row justify-content-center align-items-center w-100 h-25">
                     <h1 className="music-db-header">MUSIC-DB</h1>
+                </div>
+                <div className="row justify-content-center align-items-center w-100 h-50">
+                    <h1>
+                        <a href="https://github.com/SondosSamii"
+                            className="text-white">Sondos Sami Abuzeid
+                        </a>
+                    </h1>
                 </div>
             </div>
         </div>
