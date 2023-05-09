c3c1936c5ff5d6d0b29f36ba0ad1eb197d2f2f2f
Sondos Sami <sondos.sami.abuzeid@gmail.com>
2023-05-09 23:14:45 +0300

    Edit Routes

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,19 +1,25 @@
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
-import Home from './home';
-import Artist from './artist';
-import NotFound from './not-found';
+import Home from "./home";
+import Artist from "./artist";
+import NotFound from "./not-found";
 
-const Routes = ()=> {
-    return(
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Home}></Route>
-                <Route path="/artist/:id" component={Artist}></Route>
-                <Route path="*" component={NotFound}></Route>                    
-            </Switch>
-        </Router>
-    )
-}
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" render={() => <Redirect to="music-db" />}></Route>
+        <Route path="/music-db" component={Home}></Route>
+        <Route path="/artist/:id" component={Artist}></Route>
+        <Route path="*" component={NotFound}></Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default Routes;
